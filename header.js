3245233ae36a028384e3bb344b4bc50771e5eb3b
jrossi-santiago <josephrossi613@gmail.com>
2026-04-21 08:46:17 -0400

    Create header.js

---
+++ header.js
@@ -0,0 +1,33 @@
+(function() {
+
+  var links = [
+    { label: 'Home',        href: 'https://www.josephrossi.co' },
+    { label: 'SOP',         href: 'https://www.josephrossi.co/sop' },
+    { label: 'Motivation',  href: 'https://www.josephrossi.co/motivation' },
+    { label: 'Affirmations',href: 'https://www.josephrossi.co/affirmations' },
+    { label: 'Bookmarks',   href: 'https://www.josephrossi.co/bookmarks' },
+  ];
+
+  // Figure out which page we're on so we can mark it active
+  var currentPath = window.location.pathname.replace(/\/$/, '') || '/';
+
+  var navHTML = links.map(function(link) {
+    var linkPath = link.href.replace('https://www.josephrossi.co', '') || '/';
+    var isActive = currentPath === linkPath || (linkPath !== '/' && currentPath.startsWith(linkPath));
+    return '<a href="' + link.href + '"' + (isActive ? ' class="active"' : '') + '>' + link.label + '</a>';
+  }).join('');
+
+  var headerHTML =
+    '<header>' +
+      '<div class="header-top">' +
+        '<a href="https://josephrossi.co" class="header-home">Never Stop</a>' +
+        '<span class="header-handle">@thejosephrossi</span>' +
+      '</div>' +
+      '<nav class="header-nav">' + navHTML + '</nav>' +
+    '</header>';
+
+  // Write the header into a div with id="site-header" on the page
+  var target = document.getElementById('site-header');
+  if (target) target.innerHTML = headerHTML;
+
+})();
